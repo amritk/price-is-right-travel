44d2866fc566b28405933c5b7105313dd9673a0c
Amrit K <amrit@hockey-community.com>
2021-01-16 00:07:10 -0800

    Feat: added some temp elements to the search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,13 +1,18 @@
 import './SearchBar.css'
 import Autocomplete from '@material-ui/lab/Autocomplete'
-import TextField from '@material-ui/core/TextField'
+import { Button, TextField } from '@material-ui/core'
 import React from 'react'
 
 class SearchBar extends React.Component {
   constructor() {
     super ()
     this.state = {
-      cities: []
+      cities: [],
+      destination: null,
+      startDate: '',
+      endDate: '',
+      minPrice: null,
+      maxPrice: null
     }
   }
 
@@ -27,6 +32,9 @@ class SearchBar extends React.Component {
   }
 
   render () {
+
+    console.log(this.state)
+
     return (
       <div className="SearchBar">
 
@@ -39,12 +47,58 @@ class SearchBar extends React.Component {
           renderInput={(params) =>  
             <TextField {...params} 
               label="Destination" 
-              variant="outlined" 
+              autoFocus="true"
+              variant="standard" 
+              onChange={ ev => this.setState({destination: ev.target.value })}
             />
           }
         />
 
-        
+        {/* Min Price */}
+        <TextField
+          label="Minimum Price" 
+          type="number" 
+          step="0.01"
+          onChange={ ev => this.setState({ minPrice: ev.target.value })}
+          variant="standard" 
+        />
+
+        {/* Max Price */}
+        <TextField
+          label="Maximum Price" 
+          type="number" 
+          step="0.01"
+          onChange={ ev => this.setState({ maxPrice: ev.target.value })}
+          variant="standard" 
+        />
+
+        {/* Start Date */}
+        <TextField
+          label="Trip Start date"
+          type="date"
+          onChange={ ev => this.setState({ startDate: ev.target.value })}
+          value={this.state.startDate} 
+          variant="standard" 
+          InputLabelProps={{
+            shrink: true
+          }}
+        />
+
+        {/* Execute the search */}
+        <Button 
+          disabled={ 
+            !this.state.destination ||
+            !this.state.startDate.length ||
+            !this.state.endDate.length ||
+            !this.state.maxPrice ||
+            !this.state.minPrice && 
+            this.state.minPrice !== 0
+          } 
+          variant="contained" 
+          color="primary"
+        >
+          Search
+        </Button>
       </div>
       )
     }
