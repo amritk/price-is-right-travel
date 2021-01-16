025a6b79bc712bbacad4d4f7fa52f5aacbce08d5
Amrit K <amrit@hockey-community.com>
2021-01-15 19:57:22 -0800

    Chore: reorganized folder structure

---
+++ src/components/app/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import Button from '@material-ui/core/Button'
+import backgroundImage from '../../assets/background.jpg'
+
+const sectionStyle = {
+  backgroundImage: `url(${backgroundImage})`
+}
+
+function App() {
+  return (
+    <div className="app" style={ sectionStyle }>
+
+    </div>
+  )
+}
+
+export default App
